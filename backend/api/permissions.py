68c393c841f9c78b1622bd291d979b64c122403c
Ольга Скрябина <ibonish@Olgas-MacBook-Air.local>
2023-08-01 23:15:23 +0300

    pep8

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -16,7 +16,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         return (
             request.method in permissions.SAFE_METHODS
             or request.user.is_authenticated
-            )
+        )
 
     def has_object_permission(self, request, view, obj):
         return obj.author == request.user
