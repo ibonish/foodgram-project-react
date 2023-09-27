0fc07ba6bca0f93571828fd3097b0a283108d4a1
Ольга Скрябина <ibonish@Olgas-MacBook-Air.local>
2023-09-27 23:18:59 +1100

    debug

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -9,7 +9,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='secret-key')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.getenv('DEBUG', default='False') == 'True')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='*').split(',')
