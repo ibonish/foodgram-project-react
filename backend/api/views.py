e95f70e2d4bcc72532a319c10a410a79cf41f085
Ольга Скрябина <ibonish@Olgas-MacBook-Air.local>
2023-09-27 17:59:50 +1100

    pep8

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,4 @@
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
 from rest_framework import filters, status, viewsets
